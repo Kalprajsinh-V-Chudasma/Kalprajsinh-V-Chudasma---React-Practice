cd926234635bd02b76930f0c84a7807a51c971eb
Kalprajsinh-V-Chudasma <kvchudasama03@gmail.com>
2026-03-29 14:34:58 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/ui/app-sidebar"
 
 function App() {
 
@@ -6,8 +7,19 @@ function App() {
   return (
     <>
     <h1>hello</h1>
+
     </>
   )
 }
 
-export default App
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  )
+}
